9d13018f88303ccf22b37eb81bacc1af582264bb
Ederson Torresini <boidacarapreta@gmail.com>
2018-04-26 14:49:20 -0300

    Fim de jogo
    Para terminar o jogo: apertar A+B ou chegar a 10 frutas coletadas.

--- cobra/cobra.js
+++ cobra/cobra.js
@@ -11,6 +11,9 @@ function novaFruta() {
     fruta = game.createSprite(Math.random(5), Math.random(5))
     fruta.set(LedSpriteProperty.Blink, Math.random(11))
 }
+input.onButtonPressed(Button.AB, () => {
+    game.gameOver()
+})
 cobra = game.createSprite(0, 0)
 velocidade = 1000
 novaFruta()
@@ -24,6 +27,8 @@ basic.forever(() => {
         game.addScore(1)
         if (velocidade > 100) {
             velocidade = velocidade - 100
+        } else {
+            game.gameOver()
         }
     }
     basic.pause(velocidade)
