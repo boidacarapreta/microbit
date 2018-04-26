2c589fdcf63f634bbddd55b6eca09a701738e44d
Ederson Torresini <boidacarapreta@gmail.com>
2018-04-26 14:38:34 -0300

    Outra versão do jogo
    Ao invés de fugir de um oponente, na nova versão a cobra deverá coletar as frutas espalhadas no tabuleiro.

--- cobra/cobra.js
+++ cobra/cobra.js
@@ -1,5 +1,5 @@
-let direção = 0
-let meliante: game.LedSprite = null
+let velocidade = 0
+let fruta: game.LedSprite = null
 let cobra: game.LedSprite = null
 input.onButtonPressed(Button.A, () => {
     cobra.turn(Direction.Left, 90)
@@ -7,25 +7,24 @@ input.onButtonPressed(Button.A, () => {
 input.onButtonPressed(Button.B, () => {
     cobra.turn(Direction.Right, 90)
 })
+function novaFruta() {
+    fruta = game.createSprite(Math.random(5), Math.random(5))
+    fruta.set(LedSpriteProperty.Blink, Math.random(11))
+}
 cobra = game.createSprite(0, 0)
-cobra.set(LedSpriteProperty.Blink, 1)
-meliante = game.createSprite(4, 4)
+velocidade = 1000
+novaFruta()
+game.setScore(0)
 basic.forever(() => {
     cobra.ifOnEdgeBounce()
-    meliante.ifOnEdgeBounce()
     cobra.move(1)
-    direção = Math.random(3)
-    if (direção == 0) {
-        meliante.turn(Direction.Left, 90)
-    } else {
-        if (direção == 2) {
-            meliante.turn(Direction.Right, 90)
+    if (cobra.isTouching(fruta)) {
+        fruta.delete()
+        novaFruta()
+        game.addScore(1)
+        if (velocidade > 100) {
+            velocidade = velocidade - 100
         }
     }
-    meliante.move(1)
-    game.addScore(1)
-    if (cobra.isTouching(meliante)) {
-        game.gameOver()
-    }
-    basic.pause(1000)
+    basic.pause(velocidade)
 })
